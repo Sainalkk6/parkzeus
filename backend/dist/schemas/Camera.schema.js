d6a229dbcfe9c14a2e8525503efaef8d517551cb
sainal <sainalkk6@gmail.com>
2024-11-13 13:08:35 +0530

    chore removed dist folder

--- backend/dist/schemas/Camera.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const zod_1 = require("zod");
-const cameraSchema = zod_1.z.object({
-    port: zod_1.z.number(),
-    ip: zod_1.z.string().ip(),
-    name: zod_1.z.string()
-});
-exports.default = cameraSchema;
